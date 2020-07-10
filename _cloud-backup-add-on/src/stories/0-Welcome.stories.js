046b12426439e5cb8917fbd16f2ca96a58329d49
Nicola <nicola@highwaythreesolutions.com>
2020-07-10 10:52:31 -0700

    (ATLASSIAN-224) Icon component and update to rows in Welcome test

--- _cloud-backup-add-on/src/stories/0-Welcome.stories.js
+++ _cloud-backup-add-on/src/stories/0-Welcome.stories.js
@@ -4,9 +4,11 @@ import { Welcome } from '@storybook/react/demo';
 import ButtonProps from '../components/ButtonProps';
 import CheckboxProps from '../components/CheckboxProps';
 import EditSchedule from '../components/EditSchedule';
+import Icon from '../components/Icon';
 import Table from '../components/Table';
 import { cells } from '../components/Table';
 import { action } from '@storybook/addon-actions';
+import CheckCircleIcon from '@atlaskit/icon/glyph/check-circle';
 
 export default {
   title: 'Welcome'
@@ -34,35 +36,48 @@ class RowType {
 
 const row1 = new RowType();
 row1.date = new Date().toString();
-row1.status = 'Pending';
+row1.status = <Icon label={'Pending'} />;
 row1.size = '12KB';
 row1.option = (
-  <div>
-    <div>
-      <label>Download</label>
+  <div className="row" style={{ display: 'flex' }}>
+    <div className="column" style={{ width: '50%' }}>
+      <Icon label={''} />
     </div>
-    <div>
-      <label>Delete</label>
+    <div className="column" style={{ width: '50%' }}>
+      <Icon label={'Delete'} />
     </div>
   </div>
 );
 
 const row2 = new RowType();
 row2.date = new Date(2020, 6, 8, 14).toString();
-row2.status = 'Success';
+row2.status = <Icon label={'Completed'} />;
 row2.size = '3KB';
 row2.option = (
-  <div>
-    <div>
-      <label></label>
+  <div className="row" style={{ display: 'flex' }}>
+    <div className="column" style={{ width: '50%' }}>
+      <Icon label={'Download'} />
     </div>
-    <div>
-      <label>Delete</label>
+    <div className="column" style={{ width: '50%' }}>
+      <Icon label={'Delete'} />
     </div>
   </div>
 );
 
-const rows = [row1, row2];
+const row3 = new RowType();
+row3.date = new Date(2020, 6, 9, 14).toString();
+row3.status = <Icon label={'Errored'} />;
+row3.size = '2KB';
+row3.option = (
+  <div className="row" style={{ display: 'flex' }}>
+    <div className="column" style={{ width: '50%' }}></div>
+    <div className="column" style={{ width: '50%' }}>
+      <Icon label={'Delete'} />
+    </div>
+  </div>
+);
+
+const rows = [row1, row2, row3];
 
 //To toggle edit button: <ButtonProps label={"Edit Schedule"} buttonClicked={() => {isOpen = !isOpen;}} />
 export const toTestComponentsEditClosed = () => (
